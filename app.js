10c40ded6ede84955e711b5c8ab5676099072fcd
Praise <aypraise19@gmail.com>
2023-01-12 14:21:10 +0100

    js1

---
+++ app.js
@@ -0,0 +1,38 @@
+let LovePercentage = 100
+
+let lovers = ["Yusuf", "Alex"]
+
+let AlexLove = prompt("what is Alex's love for Yusuf")
+alert(30)
+let YusufLove = prompt("what is yusuf's love for Alex")
+alert("70")
+
+
+
+//question 2
+let array1 = [10, 20, 25, 24, 50]
+
+let array1sum = 129
+
+let array2 = [5, 4, 5, 6, 2]
+
+let array2sum = 22
+
+
+console.log(array1sum - array2sum)
+
+
+//question 3
+
+let profile = [{name: "Rache", gender: "female"}, {name: "John", gender: "male"}, {name: "Cherish", gender: "female"}, {name: "fareeda", gender: "female"}, {name: "fateema", gender: "female"}]
+
+let profileLadies = ["Rache", "Cherish", "fareeda", "fateema"]
+
+let i = ["john"]
+
+for(i = 0; i < profile.length; i++)(
+    console.log(i)
+)
+while(i = profileLadies)(
+    console.log(profileLadies)
+)
